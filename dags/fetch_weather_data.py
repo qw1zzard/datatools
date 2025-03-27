9fc4944b9fef7e4eeab79ba62cf05a8d55fe127d
Igor Pivnev <iq.pivnev@gmail.com>
2025-03-27 22:52:02 +0300

    feat!: add spark job to compute agg over temp

--- dags/fetch_weather_data.py
+++ dags/fetch_weather_data.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import os
 from datetime import datetime, timedelta
@@ -5,29 +6,39 @@ from datetime import datetime, timedelta
 import requests
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv('OPENWEATHER_API_KEY', '')
-CITIES = ['Moscow', 'Saint Petersburg', 'Novosibirsk']
-API_URL = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+API_KEY: str = os.getenv('OPENWEATHER_API_KEY', '')
+CITIES: list[str] = ['Moscow', 'Saint Petersburg', 'Novosibirsk']
+
+API_URL: str = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+CSV_PATH: str = '/opt/airflow/spark/weather_data.csv'
 
 
 def fetch_weather_data():
-    results = {}
+    results = []
+
     for city in CITIES:
         response = requests.get(API_URL.format(city=city, api_key=API_KEY))
+
         if response.status_code == 200:
-            data = response.json()
-            temp = data['main']['temp']
-            results[city] = temp
+            temp = response.json()['main']['temp']
+            results.append((city, temp))
             logging.info(f'{city}: {temp}°C')
         else:
             logging.error(
                 f'Failed to fetch weather data for {city}: {response.status_code}'
             )
-    return results
+
+    with open(CSV_PATH, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['city', 'temperature'])
+        writer.writerows(results)
+
+    logging.info(f'Data saved to {CSV_PATH}')
 
 
 default_args = {
@@ -41,8 +52,8 @@ default_args = {
 dag = DAG(
     'fetch_weather_data',
     default_args=default_args,
-    description='Сбор данных о температуре в крупных городах',
-    schedule_interval=timedelta(hours=3),
+    description='Сбор и анализ данных о температуре с помощью Spark',
+    schedule_interval=timedelta(hours=1),
     catchup=False,
 )
 
@@ -52,4 +63,12 @@ fetch_weather_task = PythonOperator(
     dag=dag,
 )
 
-fetch_weather_task
+spark_task = SparkSubmitOperator(
+    task_id='analyze_weather',
+    application='/opt/airflow/spark/analyze_weather.py',
+    conn_id='spark_default',
+    verbose=True,
+    dag=dag,
+)
+
+fetch_weather_task >> spark_task
